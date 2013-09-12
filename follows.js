6500c15500d03747ca219f87cea0c9297e3b67c3
dkingKamida <dking@kamidatech.com>
2013-09-11 21:20:55 -0400

    Update follows.js

--- follows.js
+++ follows.js
@@ -149,7 +149,6 @@
 										width: 600,
 										height: 200,
 										renderer: 'scatterplot',
-										stroke: 'true',
 										min: parseFloat(datastream.min_value) - .25*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
 										max: parseFloat(datastream.max_value) + .25*(parseFloat(datastream.max_value) - parseFloat(datastream.min_value)),
 										padding: {
